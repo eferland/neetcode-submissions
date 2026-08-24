de793c4aa9e567239dee338edd97b11ec98038ec
Ethan Ferland <99561213+eferland@users.noreply.github.com>
2026-08-24 16:19:55 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!= len(t):
+            return False
+        
+        sdict = {}
+        tdict = {}
+        for i in range(len(s)):
+            sdict[s[i]] = sdict.get(s[i], 0)+1
+            tdict[t[i]] = tdict.get(t[i], 0)+1
+        return sdict == tdict
